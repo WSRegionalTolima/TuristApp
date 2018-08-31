19b548a8b6c6903829bb8c60273f99c020c90ac4
Juancho330 <42784608+Juancho330@users.noreply.github.com>
2018-08-31 08:03:05 -0500

    prueba1

--- app/src/main/java/com/regionaltolima/worldskills/turistapp/SplashActivity.java
+++ app/src/main/java/com/regionaltolima/worldskills/turistapp/SplashActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
-
+    String slash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
